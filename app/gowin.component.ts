55ff55adfbf20caa760d5c5aab3611cfcdcafceb
o-andres-u <oscarandresu@gmail.com>
2017-03-08 23:33:05 -0500

    Preparing the application and adding some styles

--- app/gowin.component.ts
+++ app/gowin.component.ts
@@ -1,11 +1,14 @@
 import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
 
 @Component({
   selector: "gowin-app",
-  templateUrl: "",
-  styleUrls: []
+  templateUrl: "app/home.html",
+  directives: [ROUTER_DIRECTIVES]
 })
 
 export class GowinComponent {
+
+  private title: string = "gowin - Find the best place to have fun!";
 
 }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap} from "angular2/platform/browser";
 import {GowinComponent} from "./gowin.component";
+import {ROUTER_DIRECTIVES} from "angular2/router";
 
-bootstrap(GowinComponent);
+bootstrap(GowinComponent, [ROUTER_DIRECTIVES]);
